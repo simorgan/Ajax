5fd906c33b79ec69bf4999a8ed66adb147da7a93
simorgan <54029242+simorgan@users.noreply.github.com>
2020-10-22 13:40:29 +0100

    Update users.php

--- ajax-JSON/users.php
+++ ajax-JSON/users.php
@@ -3,8 +3,8 @@
 
    // Database login details
    define('DB_HOST', 'localhost');
-   define('DB_USER', 'raspi');
-   define('DB_PASS', 'Oscar2010');
+   define('DB_USER', '');
+   define('DB_PASS', '');
    define('DB_NAME', 'ajaxtest');
 
 
